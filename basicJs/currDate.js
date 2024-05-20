840ac3f5d652d82147053bbd2ed04dda45133e5b
gsmSahani <sahanigautam0446162@gmail.com>
2024-05-20 21:30:04 +0530

    1st program commited

---
+++ basicJs/currDate.js
@@ -0,0 +1,16 @@
+// write a js program to get the current date and time
+// Expected Output :
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+
+if(dd < 0){
+    dd = '0' + dd;
+}
+if(mm < 0 ){
+    mm = '0' + mm;
+}
+today = mm + '-' + dd + '-' +  yyyy;
+console.log(today);
