1f070725c434bf5b524fc3514a95266924859c7c
gsmSahani <sahanigautam0446162@gmail.com>
2024-05-21 09:11:05 +0530

    functions

--- basicJs/aoTriangle.js
+++ basicJs/aoTriangle.js
@@ -1,10 +1,17 @@
 // Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7. 
 // formula of area of triangle is 1/2 * b * h
-var length = 5;
-var breadth = 6;
-var height = 7;
+function areaOfTriangle(a, b, c) {
+    let s = (a+b+c)/2;
+    let area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    return area;
+}
+console.log(areaOfTriangle(5,6,7));
+// return areaOfTriangle;
+// var length = 5;
+// var breadth = 6;
+// var height = 7;
 
-var side = (length + breadth + height) /2;
-var area = Math.sqrt(side * (side - length) * (side - breadth) * (side * height));
-console.log(area);
-console.log(side);
+// var side = (length + breadth + height) /2;
+// var area = Math.sqrt(side * (side - length) * (side - breadth) * (side * height));
+// console.log(area);
+// console.log(side);
