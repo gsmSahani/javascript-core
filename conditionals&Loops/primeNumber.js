f6a8437ad5b3676ec2150014d3b87ae8b9221d31
gsmSahani <sahanigautam0446162@gmail.com>
2024-05-22 16:14:09 +0530

    to check prime number or not

---
+++ conditionals&Loops/primeNumber.js
@@ -0,0 +1,14 @@
+// 7. Write a JavaScript function to check if a given number is prime.
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+// var num = 20;
+console.log(isPrime(1));
