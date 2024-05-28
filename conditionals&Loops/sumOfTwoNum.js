2a6dbfb90785a933b65db72469d8f1367e1d1fc4
gsmSahani <sahanigautam0446162@gmail.com>
2024-05-28 20:28:33 +0530

    sum of two number by different mehtod basics of js

---
+++ conditionals&Loops/sumOfTwoNum.js
@@ -0,0 +1,18 @@
+// write a js program to calcluate the sum of 2 numbers
+// let a = 10;
+// let b= 50;
+// let c=  a+ b;
+// console.log(a+b);
+
+
+// using  traditional functions
+// function sumOfTwoNum(a,b){
+//     return a + b;
+// }
+// console.log(sumOfTwoNum(10,50));
+
+// using arrow functions
+
+// sumOfTwoNum = (a,b) => a +b;
+// console.log(sumOfTwoNum(20,20));
+
