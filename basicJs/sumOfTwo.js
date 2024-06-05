4abef8c795de52488305f20fe8b78e166c4ea3b6
gsmSahani <sahanigautam0446162@gmail.com>
2024-06-05 15:39:39 +0530

    sum of two number using various method

---
+++ basicJs/sumOfTwo.js
@@ -0,0 +1,16 @@
+// Write a function to find the sum of 2 numbers
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+// using functions
+// function sumOfTwo(a,b){
+//     return a + b;
+// }
+// console.log(sumOfTwo(10,50));
+
+
+    // // using arrow functions
+    // sumOfTwo = (a,b) => a+b;
+    // console.log(sumOfTwo(10,53)) ;
