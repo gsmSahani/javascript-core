83ae5df798a31c2d08c9caaedd6dfaf39568d8ac
gsmSahani <sahanigautam0446162@gmail.com>
2024-06-10 21:11:44 +0530

    using javascript currying function to filiter the array based on the even numbers from the all array elements

---
+++ javascript Currying/curriedArrayFilter.js
@@ -0,0 +1,25 @@
+// Problem 4: Currying with Array Filtering
+// Question: Create a curried function that filters an array based on a predicate function.
+
+// without curried version
+const originalArray = [1, 2, 5, 3, 4, 10];
+const filterArray = originalArray.filter((num) => num % 2 === 0);
+console.log(filterArray);
+
+// curried version
+// const filterArray1  = predict => array => array.filter(predict);
+// const isEven = num => num % 2 === 0;
+// const filterEvenNumber = filterArray1(isEven);
+// console.log(filterEvenNumber([1,2,3,4,5,6]));
+
+const filterArray2 = (predict) => {
+  return function (array) {
+    return array.filter(predict);
+  };
+};
+
+function isEven(num){
+     return num % 2 === 0;
+}
+const filterEvenNumber = filterArray2(isEven);
+console.log(filterEvenNumber([1,2,3,4,5,6,7,8,9,10]));
