5bc9f6e24376b85c2a855643625b8b4e4841dc31
gsmSahani <sahanigautam0446162@gmail.com>
2024-06-25 15:52:18 +0530

    initial commit

---
+++ 50 Coding Problems Javascript/sumOfTwoNum.js
@@ -0,0 +1,9 @@
+// write a js function to calculate the sum of 2 numbers
+function sumOfTwoNum(a, b) {
+  return a + b;
+}
+console.log(sumOfTwoNum(10, 5));
+
+// using arrow functios
+const arrowSum = (c, d) => c + d;
+console.log(arrowSum(10, 50));
---
+++ strings/countWordsString.js
@@ -0,0 +1,6 @@
+// count the words in a string
+
+function countWords(str){
+    return str.split('').length;
+}
+console.log(countWords("gautam sahani"));
